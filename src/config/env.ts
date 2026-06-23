bbdab428dea0b036e2a3961279567976f6a4764c
harish.sharma <harish.sharma@PC04>
2026-06-23 14:56:44 +0530

    Fixed Remove extra file form my projet

--- src/config/env.ts
+++
@@ -1,9 +0,0 @@
-export const API_BASE_URL: string =
-  (import.meta.env.PUBLIC_API_BASE_URL as string) || 'http://localhost:3000';
-
-export const ENV: string =
-  (import.meta.env.PUBLIC_ENV as string) || 'development';
-
-export default { API_BASE_URL, ENV };
-
-
